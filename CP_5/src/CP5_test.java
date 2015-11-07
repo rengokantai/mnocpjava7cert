f13a457c769eafacd7bac624f5fa5346a58ffac9
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-06 23:20:13 -0500

    update to p375

--- CP_5/src/CP5_test.java
+++ CP_5/src/CP5_test.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Created by Hernan Y.Ke on 2015/11/5.
@@ -13,6 +14,20 @@ public class CP5_test {
         Scanner scanner = new Scanner("The \tprogrammer");
         while(scanner.hasNext()){
             System.out.println(scanner.next());
+        }
+        System.out.println("======P373=====");
+
+        //P373
+        Scanner scanner2 = new Scanner("The \tprogrammer22me");
+        scanner2.useDelimiter("[\\d]+");
+        while(scanner2.hasNext()){
+            System.out.println(scanner2.next());
+        }
+
+        //P375
+        StringTokenizer stn = new StringTokenizer("start startup");
+        while (stn.hasMoreTokens()){
+            System.out.println(stn.nextToken());
         }
     }
 }
